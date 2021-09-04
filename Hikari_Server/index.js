c6b632b58a0cdb18993c8efe57a9ecc0f7353dac
Fedoraer <fedoraerb@gmail.com>
2021-09-04 17:02:21 +0800

    Add files via upload

--- Hikari_Server/index.js
+++ Hikari_Server/index.js
@@ -148,6 +148,65 @@ function save_result_to_db(rid, pid, uid, code, stat, pts, detail) {
   });
 }
 
+/**
+ * 将评测记录保存至Valid库
+ * @param {integer} rid : 待保存的rid
+ */
+function save_result_to_valid(rid, code, input, output) {
+  var con = mysql.createConnection({
+    host: dbcfg.host,
+    user: dbcfg.user,
+    password: dbcfg.password,
+    database: dbcfg.database,
+  });
+  con.connect();
+  var sql =
+    "INSERT INTO `reliable_judge` (rid,code,input,output) VALUES (" +
+    rid +
+    ",'" +
+    code +
+    "','" +
+    input +
+    "','" +
+    output +
+    "')";
+
+  con.query(sql, function (err) {
+    if (err) {
+      console.error(err);
+    }
+    con.end();
+  });
+}
+
+/**
+ * 生成验证代码和数据
+ * @param {function} callback : 回调函数
+ */
+function generate_validate_code(callback) {
+  var con = mysql.createConnection({
+    host: dbcfg.host,
+    user: dbcfg.user,
+    password: dbcfg.password,
+    database: dbcfg.database,
+  });
+  con.connect();
+  var sql = "SELECT * FROM `reliable_judge`";
+
+  con.query(sql, function (err, result) {
+    if (err) {
+      return data;
+    }
+    con.end();
+
+    var ccid = Math.floor(Math.random() * result.length);
+    callback({
+      code: result[ccid]["code"],
+      input: result[0]["input"],
+      output: result[0]["output"],
+    });
+  });
+}
 /*
  * socket主进程
  */
@@ -195,18 +254,26 @@ io.sockets.on("connection", function (socket) {
           for (i = 1; i <= tot_grp; i++) {
             get_problem_data(pid, i, function (c_data, grp_id) {
               get_problem_limits(pid, function (lim_data) {
-                result_list[cur_rid].grp_rec[grp_id] = new Object();
-                result_list[cur_rid].grp_rec[grp_id].exist = false;
-                socket.emit("judge_pull", {
-                  rid: cur_rid,
-                  uid: uid,
-                  pid: pid,
-                  grp: grp_id,
-                  code: code,
-                  input: c_data.input,
-                  output: c_data.output,
-                  time_limit : lim_data.time_limit,
-                  mem_limit : lim_data.mem_limit
+                generate_validate_code(function (_valid) {
+                  result_list[cur_rid].grp_rec[grp_id] = new Object();
+                  result_list[cur_rid].grp_rec[grp_id].exist = false;
+                  result_list[cur_rid].grp_rec[grp_id].valid_code = _valid.code;
+                  result_list[cur_rid].grp_rec[grp_id].valid_in = _valid.input;
+                  result_list[cur_rid].grp_rec[grp_id].valid_out =
+                    _valid.output;
+                  socket.emit("judge_pull", {
+                    rid: cur_rid,
+                    uid: uid,
+                    pid: pid,
+                    grp: grp_id,
+                    code: code,
+                    input: c_data.input,
+                    output: c_data.output,
+                    time_limit: lim_data.time_limit,
+                    mem_limit: lim_data.mem_limit,
+                    valid_code: _valid.code,
+                    valid_in: _valid.input,
+                  });
                 });
               });
             });
@@ -217,43 +284,57 @@ io.sockets.on("connection", function (socket) {
   });
 
   socket.on("judge_push_result", function (data) {
-    if (!result_list[data.rid].grp_rec[data.grp].exist) {
-      result_list[data.rid].cnt += 1;
-      result_list[data.rid].grp_rec[data.grp].exist = true;
-      result_list[data.rid].grp_rec[data.grp].status = data.status;
-      result_list[data.rid].grp_rec[data.grp].pts = data.pts;
-      result_list[data.rid].grp_rec[data.grp].out = data.out;
-      result_list[data.rid].pts += data.pts;
-      get_problem_data(data.pid, -1, function (datacnt) {
-        if (result_list[data.rid].cnt == datacnt) {
-          result_list[data.rid].stat = "AC";
-          for (i = 1; i <= datacnt; i += 1) {
-            if (result_list[data.rid].grp_rec[i].status != "AC") {
-              result_list[data.rid].stat =
-                result_list[data.rid].grp_rec[i].status;
-              break;
+    if (result_list[data.rid].grp_rec[data.grp].valid_out == data.valid_out) {
+      if (!result_list[data.rid].grp_rec[data.grp].exist) {
+        result_list[data.rid].cnt += 1;
+        result_list[data.rid].grp_rec[data.grp].exist = true;
+        result_list[data.rid].grp_rec[data.grp].status = data.status;
+        result_list[data.rid].grp_rec[data.grp].pts = data.pts;
+        result_list[data.rid].grp_rec[data.grp].out = data.out;
+        result_list[data.rid].pts += data.pts;
+        get_problem_data(data.pid, -1, function (datacnt) {
+          if (result_list[data.rid].cnt == datacnt) {
+            result_list[data.rid].stat = "AC";
+            for (i = 1; i <= datacnt; i += 1) {
+              if (result_list[data.rid].grp_rec[i].status != "AC") {
+                result_list[data.rid].stat =
+                  result_list[data.rid].grp_rec[i].status;
+                break;
+              }
             }
-          }
 
-          save_result_to_db(
-            data.rid,
-            data.pid,
-            data.uid,
-            result_list[data.rid].code,
-            result_list[data.rid].stat,
-            result_list[data.rid].pts,
-            JSON.stringify(result_list[data.rid].grp_rec)
-          );
-          socket.emit("judge_all_done", {
-            rid: data.rid,
-            uid: data.uid,
-            pid: data.pid,
-            pts: result_list[data.rid].pts,
-            datacnt: datacnt,
-            stat: result_list[data.rid].stat,
-          });
-        }
-      });
+            for (i = 1; i <= datacnt; i += 1){
+              delete result_list[data.rid].grp_rec[i].exist;
+              delete result_list[data.rid].grp_rec[i].valid_code;
+              delete result_list[data.rid].grp_rec[i].valid_in;
+              delete result_list[data.rid].grp_rec[i].valid_out;
+            }
+
+            save_result_to_db(
+              data.rid,
+              data.pid,
+              data.uid,
+              result_list[data.rid].code,
+              result_list[data.rid].stat,
+              result_list[data.rid].pts,
+              JSON.stringify(result_list[data.rid].grp_rec)
+            );
+            if (result_list[data.rid].stat == "AC"){
+              save_result_to_valid(data.rid, data.code, data.in, data.out);
+            }
+            socket.emit("judge_all_done", {
+              rid: data.rid,
+              uid: data.uid,
+              pid: data.pid,
+              pts: result_list[data.rid].pts,
+              datacnt: datacnt,
+              stat: result_list[data.rid].stat,
+            });
+          }
+        });
+      }
+    } else {
+      console.log("User " + data.pid + "Seems to be a Scam...");
     }
   });
 
